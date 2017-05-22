183917635ecf180f86ae55ac348492307fa57c23
Jeremy461 <jeremyfontijne@hotmail.com>
2017-05-22 13:21:09 +0200

    Small edit

--- dev/background.ts
+++ dev/background.ts
@@ -6,18 +6,18 @@ class Background extends GameObject {
     private character: Character;
     
     constructor(c: Character){
-        super("background", document.getElementById("container"), 0, 0);
+        super("background", document.getElementById("container"), 0, 0, 10000, 720);
         this.div.setAttribute("id", "background");
         this.character = c;
-    }
+    };
     
     public move(): void{
         this.x -= 10;
         this.div.style.transform = "translateX("+ this.x +"px)";
-    }
+    };
     
     public stop(): void{
         this.div.style.transform = "translateX("+ this.x +"px)";
-    }
+    };
     
-}
+};
